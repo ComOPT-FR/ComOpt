0e332290be52315ce8891a874847d3331a94070c
Mohamed Mahmoud <mokhairy.mahmoud@gmail.com>
2023-05-02 17:32:15 +0200

    remove boost lib

--- cmake/comoptDependencies.cmake
+++ cmake/comoptDependencies.cmake
@@ -8,14 +8,14 @@
 
 set(comopt_DEPENDENCIES)
 
-# add eigen dependency from vcpkg
+# add eigen dependency from vcpkg if use_eigen is set
 find_package(Eigen3 REQUIRED)
 message(STATUS "Eigen3 found: ${EIGEN3_INCLUDE_DIR}")
 include_directories(${EIGEN3_INCLUDE_DIR})
 
 # add boost dependency from vcpkg
-find_package(Boost REQUIRED)
-message(STATUS "Boost found: ${Boost_INCLUDE_DIRS}")
-include_directories(${Boost_INCLUDE_DIRS})
+# find_package(Boost REQUIRED)
+# message(STATUS "Boost found: ${Boost_INCLUDE_DIRS}")
+# include_directories(${Boost_INCLUDE_DIRS})
 
 
