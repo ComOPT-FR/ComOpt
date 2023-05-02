3d22808e159d3dcc560fb40e02d71faf1e7a8b13
Mohamed Mahmoud <mokhairy.mahmoud@gmail.com>
2023-05-02 16:57:22 +0200

    small bug fixed

--- cmake/comoptDependencies.cmake
+++ cmake/comoptDependencies.cmake
@@ -9,12 +9,12 @@
 set(comopt_DEPENDENCIES)
 
 # add eigen dependency from vcpkg
-find_package(Eigen3 CONFIG REQUIRED)
+find_package(Eigen3 REQUIRED)
 message(STATUS "Eigen3 found: ${EIGEN3_INCLUDE_DIR}")
 include_directories(${EIGEN3_INCLUDE_DIR})
 
 # add boost dependency from vcpkg
-find_package(Boost CONFIG REQUIRED)
+find_package(Boost REQUIRED)
 message(STATUS "Boost found: ${Boost_INCLUDE_DIRS}")
 include_directories(${Boost_INCLUDE_DIRS})
 
